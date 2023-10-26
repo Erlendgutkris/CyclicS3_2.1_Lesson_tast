c28d5a1da0b6f87fb769ee079293b8db941bdf3f
Erlendgutkris <erlendgutkris@gmail.com>
2023-10-26 15:09:07 +0200

    runtuntunt

--- routes/index.js
+++ routes/index.js
@@ -6,11 +6,11 @@ const s3 = new AWS.S3()
 /* GET home page. */
 router.get('/', async function(req, res, next){
   let my_file = await s3.getObject({
-    Bucket: process.env.cyclic-jade-fluffy-scorpion-eu-west-1,
+    Bucket: "cyclic-jade-fluffy-scorpion-eu-west-1",
     Key: "message.json"
   }).promise()
 
-  const myMessage = JSON.parse(my_file.Body)?.myMessage;
+  const myMessage = JSON.parse(my_file.Body)?.message;
 
   res.json({ 
     status: 'success',
@@ -25,7 +25,7 @@ router.post("/", async function(req, res){
   }
   await s3.putObject({
     Body: JSON.stringify(messageObj, null, 2),
-    Bucket: process.env.cyclic-jade-fluffy-scorpion-eu-west-1,
+    Bucket: "cyclic-jade-fluffy-scorpion-eu-west-1",
     Key: "message.json"
   }).promise()
   res.json({
